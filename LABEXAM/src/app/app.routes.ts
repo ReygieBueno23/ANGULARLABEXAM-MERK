af8339549bdaa76a3730365d96ac39a09f1b65d5
@ReygieBueno23 <reygieb6@gmail.com>
2024-10-11 14:55:47 +0800

    creating component
    creating component and loginform and link routerLink

--- LABEXAM/src/app/app.routes.ts
+++ LABEXAM/src/app/app.routes.ts
@@ -1,6 +1,8 @@
 import { Routes } from '@angular/router';
 import { SignupComponentComponent } from './signup-component/signup-component.component';
+import { LoginComponentComponent } from './login-component/login-component.component';
 
 export const routes: Routes = [
   { path: 'signup-component', component: SignupComponentComponent },
+  { path: 'login-component', component: LoginComponentComponent },
 ];
--- LABEXAM/src/app/signup-component/signup-component.component.ts
+++ LABEXAM/src/app/signup-component/signup-component.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-signup-component',
   standalone: true,
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './signup-component.component.html',
   styleUrl: './signup-component.component.css'
 })
