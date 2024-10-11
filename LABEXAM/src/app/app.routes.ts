1ada255a5fe325eb7bde3c137c5e78e46c65ac77
@ReygieBueno23 <reygieb6@gmail.com>
2024-10-11 14:25:25 +0800

    creating signup form and component
    creating signup form and component

---
+++ LABEXAM/src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from '@angular/router';
+import { SignupComponentComponent } from './signup-component/signup-component.component';
+
+export const routes: Routes = [
+  { path: 'signup-component', component: SignupComponentComponent },
+];
